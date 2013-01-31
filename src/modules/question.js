7dd932acc57e9afbdedfbcae5ed62c74374e92d7
TZ <atian25@qq.com>
2013-01-31 09:24:23 +0800

    增加问题页模块, 实现答案按时间排序
    Signed-off-by: TZ <atian25@qq.com>

---
+++ src/modules/question.js
@@ -0,0 +1,11 @@
+/**
+ * 问题页
+ */
+$(function(){
+  if(utils.isPath('question')){
+    //答案按时间排序
+    if(utils.getCfg('answer_orderByTime')){
+      client.click('.zh-answers-filter-popup div[data-key=added_time]');
+    }
+  }
+})
