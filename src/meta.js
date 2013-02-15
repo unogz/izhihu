381a7b65be9031e53e4eec8f99f5750f4289db0d
unogz <unogzx@gmail.com>
2013-02-15 12:11:38 +0800

    将依赖库移至import下随脚本一起打包

--- src/meta.js
+++ src/meta.js
@@ -4,9 +4,6 @@
 // @version      2.0.0
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
-// @require      http://underscorejs.org/underscore-min.js
-// @require      https://github.com/allmarkedup/jQuery-URL-Parser/raw/master/purl.js
-// @require      https://github.com/n1k0/casperjs/raw/master/modules/clientutils.js
 // @copyright    2012+, @钢盅郭子 @刘勇 @罗大睿
 // ==/UserScript==
 
