94134ac8494c46e35ffe5ed1ba7ab2d02e5ad341
unogz <unogzx@gmail.com>
2013-03-18 13:51:20 +0800

    add box shadow for RightComment

--- src/modules/question.js
+++ src/modules/question.js
@@ -30,9 +30,17 @@ var $lblQuestionMeta=$('#zh-question-meta-wrap')//question_meta
         $questionWrap.children('.meta-item[name=addcomment]')
         	.css({'display':'block','float':'right','margin-left':7})
         	.insertBefore($questionWrap.children().first());
-        processComment($('.zm-comment-box',$questionWrap));
+        if(!$('#izh_QuestionShadow').length){
+            $('<div>',{'class':'izh_boxShadow',id:'izh_QuestionShadow'}).css({
+                'z-index': '-1'
+              , 'position': 'relative'
+              , 'top': -25
+              , 'margin-left': -32
+            }).prependTo('#zh-single-question').hide();
+        }   
     }
-        
+    processComment($('.zm-comment-box',$questionWrap));
+
     //process each answer
     var $listAnswers=$('.zm-item-answer','#zh-single-question');
     if($listAnswers&&$listAnswers.length){
