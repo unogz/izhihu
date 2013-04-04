118297bf4dbc237e4336c337aa3238391c037cbc
unogz <unogzx@gmail.com>
2013-04-04 12:41:15 +0800

    Built new version

--- iZhihu for Chrome/126619.user.js
+++ iZhihu for Chrome/126619.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.4.1
+// @version      2.5.0
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
 // ==/UserScript==
-var version = "2.4.1.321";
+var version = "2.5.0";
 
-var updateDate = "2013-4-3";
+var updateDate = "2013-4-4";
 
 /*!
  * Casper is a navigation utility for PhantomJS.
--- src/meta.js
+++ src/meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.4.1
+// @version      2.5.0
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
 // ==/UserScript==
 
-var version='2.4.1.321';
-var updateDate='2013-4-3';
+var version='2.5.0';
+var updateDate='2013-4-4';
 
