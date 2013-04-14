fbf5ccc482ca4490f4823a50c1c5a24658c6d90e
unogz <unogzx@gmail.com>
2013-04-14 22:31:48 +0800

    Rebuilt version

--- src/meta.js
+++ src/meta.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.6.0
+// @version      2.5.2
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
+// @include      http://www.zhihu.com/*
 // @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
 // @grant GM_xmlhttpRequest
 // @grant GM_registerMenuCommand
 // @grant GM_openInTab
 // ==/UserScript==
-var version='2.6.0';
+
+var version='2.5.2';
 var updateDate='2013-4-14';
