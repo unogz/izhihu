e6165ce71a5488dcf85af8045c669a8818d24d81
unogz <unogzx@gmail.com>
2013-04-15 14:01:52 +0800

    Built new version 2.5.3

--- src/meta.js
+++ src/meta.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.5.2
+// @version      2.5.3
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @include      http://www.zhihu.com/*
 // @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
+// @updateinfo   发布日期 2013-04-15|# 改进「快速收藏」：美化菜单外观|# 修复错误：回答页中「快速收藏」菜单无内容
 // @grant GM_xmlhttpRequest
 // @grant GM_registerMenuCommand
 // @grant GM_openInTab
 // ==/UserScript==
 
-var version='2.5.2';
-var updateDate='2013-4-14';
+var version='2.5.3';
+var updateDate='2013-4-15';
