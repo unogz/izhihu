0423a3a39460c71ec7dae0198cc84039f96faf15
unogz <unogzx@gmail.com>
2013-04-17 15:25:10 +0800

    Built new version to 2.6.0

--- src/meta.js
+++ src/meta.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.5.5
+// @version      2.6.0
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @include      http://www.zhihu.com/*
 // @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
-// @updateinfo   发布日期 2013-04-15|# 修复错误：在新版 Chrome 上无法使用 iZhihu 扩展
+// @updateinfo   发布日期 2013-04-17|# 改进「快速收藏」：在「添加到收藏夹」时提供「置顶」选项，置顶条目将保持在「快速收藏」菜单顶部显示
 // @grant GM_xmlhttpRequest
 // @grant GM_registerMenuCommand
 // @grant GM_openInTab
 // ==/UserScript==
 
-var version='2.5.5';
+var version='2.6.0';
 var updateDate='2013-4-17';
 
 var bGreasemonkeyServiceDefined = false;
