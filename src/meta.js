89b5e61471c7d41762f625dbf46a04668d50bd6a
unogz <unogzx@gmail.com>
2013-04-17 10:49:48 +0800

    Fixed unsafeWindow not defined without GreasemonkeyService Built new version 2.5.5

--- src/meta.js
+++ src/meta.js
@@ -1,16 +1,35 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.5.4
+// @version      2.5.5
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @include      http://www.zhihu.com/*
 // @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
-// @updateinfo   发布日期 2013-04-15|# 修复错误：问题页中「回答目录」跳转失效
+// @updateinfo   发布日期 2013-04-15|# 修复错误：无法在新版 Chrome 上运行
 // @grant GM_xmlhttpRequest
 // @grant GM_registerMenuCommand
 // @grant GM_openInTab
 // ==/UserScript==
 
-var version='2.5.4';
-var updateDate='2013-4-15';
+var version='2.5.5';
+var updateDate='2013-4-17';
+
+var bGreasemonkeyServiceDefined = false;
+
+try {
+    if (typeof Components.interfaces.gmIGreasemonkeyService === "object") {
+        bGreasemonkeyServiceDefined = true;
+    }
+}
+catch (err) {
+    //Ignore.
+}
+
+if ( typeof unsafeWindow === "undefined"  ||  ! bGreasemonkeyServiceDefined) {
+    unsafeWindow    = ( function () {
+        var dummyElem   = document.createElement('p');
+        dummyElem.setAttribute ('onclick', 'return window;');
+        return dummyElem.onclick ();
+    } ) ();
+}
