a043a00c2eacf1dc4601de27e2d902f60708e29a
unogz <unogzx@gmail.com>
2013-04-19 16:42:00 +0800

    Built new version

--- src/meta.js
+++ src/meta.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.6.1
+// @version      2.6.2
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @include      http://www.zhihu.com/*
 // @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
-// @updateinfo   发布日期 2013-04-18|# 修复错误：「快速屏蔽」在评论列表中的「候审」按钮工作不正常
+// @updateinfo   发布日期 2013-04-19|# 修复：「快速收藏」菜单位置异常|# 修复：用户回答「地址清单」链接地址异常
 // @grant GM_xmlhttpRequest
 // @grant GM_registerMenuCommand
 // @grant GM_openInTab
 // ==/UserScript==
 
-var version='2.6.1';
-var updateDate='2013-4-18';
+var version='2.6.2';
+var updateDate='2013-4-19';
 
 var bGreasemonkeyServiceDefined = false;
 
