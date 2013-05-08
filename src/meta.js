3e3d49f0236a297e0f1d2fd5e4777c1dcfe5bed8
unogz <unogzx@gmail.com>
2013-05-08 23:23:58 +0800

    Built new version 2.6.4

--- src/meta.js
+++ src/meta.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.6.3
+// @version      2.6.4
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @include      http://www.zhihu.com/*
 // @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
-// @updateinfo   发布日期 2013-05-06|# 修复：开启「右显评论」后某些页面内评论列表不正常（目前除首页、问题、回答外，其余页面的评论列表恢复至知乎原有样式）
+// @updateinfo   发布日期 2013-05-08|# 修复：开启「右显评论」后当评论数较多时页面失去响应的问题
 // @grant GM_xmlhttpRequest
 // @grant GM_registerMenuCommand
 // @grant GM_openInTab
 // ==/UserScript==
 
-var version='2.6.3';
-var updateDate='2013-5-6';
+var version='2.6.4';
+var updateDate='2013-5-8';
 
 var bGreasemonkeyServiceDefined = false;
 
