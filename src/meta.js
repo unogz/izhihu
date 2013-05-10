4b857200c19f28c2bcb73a6a71f46978ad964d1d
unogz <unogzx@gmail.com>
2013-05-10 13:13:56 +0800

    Built new version 2.6.5

--- src/meta.js
+++ src/meta.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.6.4
+// @version      2.6.5
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @include      http://www.zhihu.com/*
 // @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
-// @updateinfo   发布日期 2013-05-08|# 修复：开启「右显评论」后当评论数较多时页面失去响应的问题
+// @updateinfo   发布日期 2013-05-10|# 修复：回答的 iZhihu 特性会在提交修改后消失的问题
 // @grant GM_xmlhttpRequest
 // @grant GM_registerMenuCommand
 // @grant GM_openInTab
 // ==/UserScript==
 
-var version='2.6.4';
-var updateDate='2013-5-8';
+var version='2.6.5';
+var updateDate='2013-5-10';
 
 var bGreasemonkeyServiceDefined = false;
 
