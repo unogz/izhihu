684d3500f7bbedb3ac10937b878f10226f3b8926
unogz <unogzx@gmail.com>
2013-05-12 20:39:02 +0800

    Built new version 2.6.7

--- src/meta.js
+++ src/meta.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.6.6
+// @version      2.6.7
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @include      http://www.zhihu.com/*
 // @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
-// @updateinfo   发布日期 2013-05-11|# 改进「问题页」：当回答总数（含折叠区）超过 100 时，将提示是否加载 iZhihu
+// @updateinfo   发布日期 2013-05-12|# 改进「问题页」：当回答总数（含折叠区）超过 100 时，将提示是否关闭 iZhihu「回答目录」
 // @grant GM_xmlhttpRequest
 // @grant GM_registerMenuCommand
 // @grant GM_openInTab
 // ==/UserScript==
 
-var version='2.6.6';
-var updateDate='2013-5-11';
+var version='2.6.7';
+var updateDate='2013-5-12';
 
 var bGreasemonkeyServiceDefined = false;
 
