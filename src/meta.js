e9f9760f36d117488f8d71369debc2540fda1942
unogz <unogzx@gmail.com>
2013-05-20 14:12:42 +0800

    Built beta version 2.7.0.370

--- src/meta.js
+++ src/meta.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.6.7
+// @version      2.7.0 Beta
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @include      http://www.zhihu.com/*
 // @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
-// @updateinfo   发布日期 2013-05-12|# 改进「问题页」：当回答总数（含折叠区）超过 100 时，将提示是否关闭 iZhihu「回答目录」
+// @updateinfo   发布日期 2013-05-X|# 改进「右舷评论」：大幅改善了评论列表的外观和性能
 // @grant GM_xmlhttpRequest
 // @grant GM_registerMenuCommand
 // @grant GM_openInTab
 // ==/UserScript==
 
-var version='2.6.7';
-var updateDate='2013-5-12';
+var version='2.7.0';
+var updateDate='2013-5-X';
 
 var bGreasemonkeyServiceDefined = false;
 
