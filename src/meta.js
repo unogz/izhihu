0f8daa59c0e16da8343d55ac9ff2e76f9b797f08
unogz <unogzx@gmail.com>
2013-05-21 10:41:16 +0800

    Ready for release version 2.7.0.372

--- src/meta.js
+++ src/meta.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.7.0 Beta
+// @version      2.7.0
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @include      http://www.zhihu.com/*
 // @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
-// @updateinfo   发布日期 2013-05-X|# 改进「右舷评论」：大幅改善了评论列表的外观和性能
+// @updateinfo   发布日期 2013-05-21|# 大幅改善了「右舷评论」的外观和性能。更新内容包括：|> 调整评论列表的位置，吸附在页面主要内容（时间线条目、问题及回答）右侧，并不受页面缩放及横向卷动影响|> 当窗口宽度适中时将向左侧滑以保证评论列表展开空间|> 页面纵向卷动时，评论列表以动画形式上下平移|> 细节上的完善
 // @grant GM_xmlhttpRequest
 // @grant GM_registerMenuCommand
 // @grant GM_openInTab
 // ==/UserScript==
 
 var version='2.7.0';
-var updateDate='2013-5-X';
+var updateDate='2013-5-21';
 
 var bGreasemonkeyServiceDefined = false;
 
