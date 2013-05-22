0586477fc7604ccee1fa4576c6004c67dd391bb9
unogz <unogzx@gmail.com>
2013-05-23 00:47:59 +0800

    Built hotfix version 2.7.2.376

--- src/meta.js
+++ src/meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.7.1
+// @version      2.7.2
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @include      http://www.zhihu.com/*
@@ -12,7 +12,7 @@
 // @grant GM_openInTab
 // ==/UserScript==
 
-var version='2.7.1';
+var version='2.7.2';
 var updateDate='2013-5-21';
 
 var bGreasemonkeyServiceDefined = false;
