ebe051bbd4abaca153931aa933d73fb98d7c19af
unogz <unogzx@gmail.com>
2013-08-05 17:26:21 +0800

    Built 2.9.0.295

--- src/meta.js
+++ src/meta.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.8.1
+// @version      2.9
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @include      http://www.zhihu.com/*
 // @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
-// @updateinfo   发布日期 2013-08-02|# 改进「首页」# 修复了一些因首页改版引起的界面异常
+// @updateinfo   发布日期 2013-08-02|# 改进「首页」|# 改进「收藏、回答地址清单」
 // @grant GM_xmlhttpRequest
 // @grant GM_registerMenuCommand
 // @grant GM_openInTab
 // ==/UserScript==
 
-var version='2.8.1';
-var updateDate='2013-8-2';
+var version='2.9';
+var updateDate='2013-8-X';
 
 var bGreasemonkeyServiceDefined = false;
 
