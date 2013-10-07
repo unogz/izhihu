2886fdcd9106f0f43d712ea108f2d10e199657d0
unogz <unogzx@gmail.com>
2013-10-07 13:07:15 +0800

    Updated meta info

--- src/meta.js
+++ src/meta.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         iZhihu
-// @namespace    https://github.com/unogz/izhihu
+// @namespace    http://zhuanlan.zhihu.com/izhihu
 // @version      3.0
-// @description  知乎插件
+// @description  「知乎」自定义脚本
 // @match        http://www.zhihu.com/*
 // @include      http://www.zhihu.com/*
-// @copyright    2013+, @钢盅郭子 @刘勇 @罗大睿
+// @icon         http://ilovezhihu.duapp.com/logo.png
+// @copyright    2013+, https://github.com/unogz/izhihu
 // @updateinfo   发布日期 2013-10-X|
 // @grant GM_xmlhttpRequest
 // @grant GM_registerMenuCommand
