00322621169a1638523b75c97e9bcffadc6150a3
unogz@office <unogzx@gmail.com>
2014-06-11 13:39:23 +0800

    Built fix 2.10.14

--- src/meta.js
+++ src/meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iZhihu
 // @namespace    https://github.com/unogz/izhihu
-// @version      2.10.13
+// @version      2.10.14
 // @description  知乎插件
 // @match        http://www.zhihu.com/*
 // @include      http://www.zhihu.com/*
