18a84dcc78c6b9cb430596d7bf3dc744f1a7f3f2
unogz@office <unogzx@gmail.com>
2014-07-28 21:59:43 +0800

    - Fixed QuickBlock button for Article - Removed dump codes

--- src/modules/Answer.js
+++ src/modules/Answer.js
@@ -65,15 +65,14 @@ function Answer(iZhihu) {
         });
         if(iZhihu.QuickBlock){
             // Region: 快速屏蔽
-            var $voteMore=$('.zm-item-vote-info > .more',$a);
+            var $voteMore=$('.zm-item-vote-info > .more',$a)
             if($voteMore.length){
                 $voteMore.parent().parent().bind('DOMNodeInserted',function(event){
                     var $vi=$(event.target).filter('.zm-item-vote-info')
-                    console.log($vi.children().first().children())
                     if (!$vi.length || $vi.attr('data-votecount')!=$vi.children().first().children().length)
                         return
-                    iZhihu.QuickBlock.addQuickBlock($vi,iZhihu.QuickBlock);
-                });
+                    iZhihu.QuickBlock.addQuickBlock($vi)
+                })
             }
             // Region end
         }
