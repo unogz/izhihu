17ec72ae7029ed1b985cacba0e255ba26486e1ce
unogz <unogzx@gmail.com>
2014-09-02 00:08:06 +0800

    Replaced HTML insertion by DOM building

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -2,86 +2,166 @@
  * 配置界面
  */
 
-    var domBtnSettings = [
-        '<li>',
-          '<a href="javascript:void(0);" tabindex="-1">',
-            '<i class="zg-icon zg-icon-dd-settings izhihu-settings"></i>',
-            'iZhihu',
-          '</a>',
-        '</li>'
-    ].join('')
-
     var cbemptyimg = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAMAAAAoyzS7AAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAAZQTFRFAAAAAAAApWe5zwAAAAF0Uk5TAEDm2GYAAAAMSURBVHjaYmAACDAAAAIAAU9tWeEAAAAASUVORK5CYII='
-    var domDlgSettings = [
-      '<div id="izh-dlg-settings" class="modal-dialog" tabindex="0" style="display:none;width:600px">',
-        '<div class="modal-dialog-title modal-dialog-title-draggable">',
-          '<span class="modal-dialog-title-text">配置选项</span>',
-          '<span class="modal-dialog-title-close"></span>',
-        '</div>',
-        '<div class="modal-dialog-content">',
-          '<div>',
-            '<div class="zg-section">',
-            '<table class="t_set_tb"border="0"cellspacing="0"cellpadding="5"width="100%">',
-            	'<thead>',
-            		'<tr><td colspan="6"align="left"><b>功能开关</b>（更改后设置将立刻保存，但只有当页面再次打开时才会生效)</td></tr>',
-            	'</thead>',
-            	'<tbody>',
-                '<tr style="display:none"><td colspan="4"align="left"title="">在首页直接浏览常去话题</td><td width="1px"align="right"></td><td width="1px"align="right"><label><input type="checkbox" class="izh-check" id="iZhihu_setHomeTopics" name="HomeTopics" />aaa</label></td></tr>',
-                '<tr><td colspan="4"align="left"title="">改变网页样式外观</td><td align="right"><i class="icon icon-help"data-tip="s$t$* 首页隐藏大头像<br/>* 缩进投票按钮（问题/回答页）<br/>* 按钮图标动画 "></i></td><td align="right"><input type="checkbox" class="izh-check" id="iZhihu_setHomeLayout" name="HomeLayout" /></td></tr>',
-                '<tr><td colspan="4"align="left"title="">调整首页「新动态」提醒按钮</td><td align="right"><i class="icon icon-help"data-tip="s$t$挪到 Timeline 右上角<br/>与标题「最新动态」平行"></i></td><td align="right"><input type="checkbox" class="izh-check" id="iZhihu_setHomeNoti" name="HomeNoti" /></td></tr>',
-                '<tr><td colspan="4"align="left"title="">开启「首页分栏」</td><td align="right"><i class="icon icon-help"data-tip="s$t$将首页动态分类单独显示：<br/>问题、回答、专栏"></i></td><td align="right"><input type="checkbox" class="izh-check" id="iZhihu_setHomeFeedsColumns" name="HomeFeedsColumns" /></td></tr>',
-                '<tr><td colspan="4"align="left">将用户信息挪到回答下方</td><td align="right"></td><td align="right"><input type="checkbox" class="izh-check" id="iZhihu_setAuthorRear" name="AuthorRear" /></td></tr>',
-                '<tr><td align="left">开启「右舷评论」</td><td align="right"><i class="icon icon-help"data-tip="s$t$在页面右侧浮动显示打开的评论列表<br/>在首页、问题、回答页中生效"></i></td><td width="1px"align="right"><input type="checkbox" class="izh-check" id="iZhihu_setRightComment" name="ShowComment" /></td>',
-                '<td width="1px"align="left"nowrap>关闭时自动卷屏至对应条目</td><td align="right"><i class="icon icon-help"data-tip="s$t$仅对右舷评论生效"></i></td><td width="1px"align="right"><input type="checkbox" class="izh-check" id="iZhihu_setRightComment_AutoScrollPageWhenClosing" name="RightComment_AutoScrollPageWhenClosing" /></td></tr>',
-                '<tr><td colspan="4"align="left"title="">开启「快速屏蔽」（加入黑名单/取消关注）功能</td><td align="right"><i class="icon icon-help"data-tip="s$t$在赞同列表、评论列表中使用"></i></td><td align="right"><input type="checkbox" class="izh-check" id="iZhihu_setQuickBlock" name="QuickBlock" /></td></tr>',
-                '<tr><td colspan="4"align="left"title="">开启「快速收藏」菜单</td><td align="right"><i class="icon icon-help"data-tip="s$t$鼠标移上「收藏」按钮时显示"></i></td><td align="right"><input type="checkbox" class="izh-check" id="iZhihu_setQuickFavo" name="QuickFavo" /></td></tr>',
-                '<tr><td colspan="4"align="left"title="">开启「回答目录」</td><td align="right"><i class="icon icon-help"data-tip="s$t$在问题页面左侧掩藏，鼠标移上时展开<br/>并在右侧即时显示回答预览"></i></td><td align="right"><input type="checkbox" class="izh-check" id="iZhihu_setAuthorList" name="AuthorList" /></td></tr>',
-            	'</tbody>',
-            '</table>',
-            '</div>',
-              '<div class="zg-left">',
-                '当前版本：'+version+'；',
-                '最后更新：'+updateDate,
-                '<br/>制作：',
-                '<a data-tip="p$t$unogzx" href="/people/unogzx">@钢盅郭子</a>，',
-                '<a data-tip="p$t$liuyong25" href="/people/liuyong25">@天猪(刘勇)</a>，',
-                '<a data-tip="p$t$luoxr" href="/people/luoxr">@yukirock</a>，',
-                '<a data-tip="p$t$morlay" href="/people/morlay">@墨磊</a>',
-                '<br/>感谢：',
-                '<a data-tip="p$t$PeterDeng" href="/people/PeterDeng">@邓文博</a>，<a data-tip="p$b$cakvfcwz" href="/people/cakvfcwz">@水云逸</a>',
-              '</div>',
-              '<div class="zm-command">',
-              '<a id="izhRefresh" class="zg-btn-blue" href="javascript:void(0);">刷新网页</a>',
-            '</div>',
-          '</div>',
-        '</div>',
-      '</div>'
-    ].join('')
+    var $btnSettings = $('<li>')
+            .append($('<a>', { href: 'javascript:void(0);', tabindex: '-1' })
+                .append($('<i>', { 'class': 'zg-icon zg-icon-dd-settings izhihu-settings' }))
+                .append('iZhihu')
+            ).click(function(){
+                var $settings = $('#izh-dlg-settings')
+                $('.modal-dialog-bg').show()
+                $('input.izh-check', '#izh-dlg-settings').each(function(i, e){
+                    var key = e.getAttribute('name')
+                      , value = utils.getCfg(key)
+                    if(value)
+                        $(e).iCheck('check')
+                })
+                $settings.css({
+                    'z-index':'89'
+                  , 'position':'fixed'
+                  , 'top': ($win.height() - $settings.height()) / 2
+                  , 'left': ($win.width() - $settings.width()) / 2
+                }).fadeIn('slow')
+            }).insertBefore($('ul#top-nav-profile-dropdown li:last'))
+      , $dlgSettings = $('<div>', { id: 'izh-dlg-settings', 'class': 'modal-dialog', tabindex: '0', style: 'display:none;width:600px'})
+            .append($('<div>', { 'class': 'modal-dialog-title modal-dialog-title-draggable' })
+                .append($('<span>', { 'class': 'modal-dialog-title-text', 'html': '配置选项' }))
+                .append($('<span>', {
+                    'class': 'modal-dialog-title-close'
+                  , click: function(){
+                        $('.modal-dialog-bg').hide()
+                        $('#izh-dlg-settings').first().hide()
+                    }
+                }))
+            )
+            .append($('<div>', { 'class': 'modal-dialog-content' })
+                .append($('<div>', {})
+                    .append($('<div>', { 'class': 'zg-section' })
+                        .append($('<table>', { 'class': 't_set_tb', border: 0, cellspacing: 0, cellpadding: 5, width: '100%' })
+                            .append($('<thead>', {})
+                                .append($('<tr>', {})
+                                    .append($('<td>', { colspan: 6, align: 'left', html: '<b>功能开关</b>（更改后设置将立刻保存，但只有当页面再次打开时才会生效）' }))
+                                )
+                            )
+                            .append($('<tbody>', {})
+/*
+                                .append($('<tr>', {})
+                                    .append($('<td>', { colspan: 4, align: 'left', html: '在首页直接浏览常去话题' }))
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<i>', { 'class': 'icon icon-help', 'data-tip': ""}))
+                                    )
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<input>', { type: "checkbox", 'class': "izh-check", id: "iZhihu_setHomeTopics", name: "HomeTopics" }))
+                                    )
+                                )
+*/
+                                .append($('<tr>', {})
+                                    .append($('<td>', { colspan: 4, align: 'left', html: '改变网页样式外观' }))
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<i>', { 'class': 'icon icon-help', 'data-tip': "s$t$* 首页隐藏大头像<br/>* 缩进投票按钮（问题/回答页）<br/>* 按钮图标动画 "}))
+                                    )
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<input>', { type: "checkbox", 'class': "izh-check", id: "iZhihu_setHomeLayout", name: "HomeLayout" }))
+                                    )
+                                )
+                                .append($('<tr>', {})
+                                    .append($('<td>', { colspan: 4, align: 'left', html: '调整首页「新动态」提醒按钮' }))
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<i>', { 'class': 'icon icon-help', 'data-tip': "s$t$挪到 Timeline 右上角<br/>与标题「最新动态」平行"}))
+                                    )
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<input>', { type: "checkbox", 'class': "izh-check", id: "iZhihu_setHomeNoti", name: "HomeNoti" }))
+                                    )
+                                )
+                                .append($('<tr>', {})
+                                    .append($('<td>', { colspan: 4, align: 'left', html: '开启「首页分栏」' }))
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<i>', { 'class': 'icon icon-help', 'data-tip': "s$t$将首页动态分类单独显示：<br/>问题、回答、专栏"}))
+                                    )
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<input>', { type: "checkbox", 'class': "izh-check", id: "iZhihu_setHomeFeedsColumns", name: "HomeFeedsColumns" }))
+                                    )
+                                )
+                                .append($('<tr>', {})
+                                    .append($('<td>', { colspan: 4, align: 'left', html: '将用户信息挪到回答下方' }))
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<i>', { 'class': 'icon icon-help', 'data-tip': ""}))
+                                    )
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<input>', { type: "checkbox", 'class': "izh-check", id: "iZhihu_setAuthorRear", name: "AuthorRear" }))
+                                    )
+                                )
+                                .append($('<tr>', {})
+                                    .append($('<td>', { align: 'left', html: '开启「右舷评论」' }))
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<i>', { 'class': 'icon icon-help', 'data-tip': "s$t$在页面右侧浮动显示打开的评论列表<br/>在首页、问题、回答页中生效"}))
+                                    )
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<input>', { type: "checkbox", 'class': "izh-check", id: "iZhihu_setRightComment", name: "ShowComment" }))
+                                    )
+                                    .append($('<td>', { width: 1, nowrap: 'nowrap', align: 'left', html: '关闭时自动卷屏至对应条目' }))
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<i>', { 'class': 'icon icon-help', 'data-tip': "s$t$仅对右舷评论生效"}))
+                                    )
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<input>', { type: "checkbox", 'class': "izh-check", id: "iZhihu_setRightComment_AutoScrollPageWhenClosing", name: "RightComment_AutoScrollPageWhenClosing" }))
+                                    )
+                                )
+                                .append($('<tr>', {})
+                                    .append($('<td>', { colspan: 4, align: 'left', html: '开启「快速屏蔽」（加入黑名单/取消关注）功能' }))
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<i>', { 'class': 'icon icon-help', 'data-tip': "s$t$在赞同列表、评论列表中使用"}))
+                                    )
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<input>', { type: "checkbox", 'class': "izh-check", id: "iZhihu_setQuickBlock", name: "QuickBlock" }))
+                                    )
+                                )
+                                .append($('<tr>', {})
+                                    .append($('<td>', { colspan: 4, align: 'left', html: '开启「快速收藏」菜单' }))
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<i>', { 'class': 'icon icon-help', 'data-tip': "s$t$鼠标移上「收藏」按钮时显示"}))
+                                    )
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<input>', { type: "checkbox", 'class': "izh-check", id: "iZhihu_setQuickFavo", name: "QuickFavo" }))
+                                    )
+                                )
+                                .append($('<tr>', {})
+                                    .append($('<td>', { colspan: 4, align: 'left', html: '开启「回答目录」' }))
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<i>', { 'class': 'icon icon-help', 'data-tip': "s$t$在问题页面左侧掩藏，鼠标移上时展开<br/>并在右侧即时显示回答预览"}))
+                                    )
+                                    .append($('<td>', { align: 'right'})
+                                        .append($('<input>', { type: "checkbox", 'class': "izh-check", id: "iZhihu_setAuthorList", name: "AuthorList" }))
+                                    )
+                                )
+                            )
+                        )
+                    )
+                    .append($('<div>', { 'class': 'zg-left' })
+                        .append('当前版本：'+version+'；')
+                        .append('最后更新：'+updateDate)
+                        .append($('<br>'))
+                        .append('制作：')
+                        .append($('<a>', { 'data-tip': "p$t$unogzx", href: "/people/unogzx", html: '@钢盅郭子' }))
+                        .append('，')
+                        .append($('<a>', { 'data-tip': "p$t$liuyong25", href: "/people/liuyong25", html: '@天猪(刘勇)' }))
+                        .append('，')
+                        .append($('<a>', { 'data-tip': "p$t$luoxr", href: "/people/luoxr", html: '@yukirock' }))
+                        .append('，')
+                        .append($('<a>', { 'data-tip': "p$t$morlay", href: "/people/morlay", html: '@墨磊' }))
+                        .append($('<br>'))
+                        .append('感谢：')
+                        .append($('<a>', { 'data-tip': "p$t$PeterDeng", href: "/people/PeterDeng", html: '@邓文博' }))
+                        .append('，')
+                        .append($('<a>', { 'data-tip': "p$t$cakvfcwz", href: "/people/cakvfcwz", html: '@水云逸' }))
+                    )
+                    .append($('<div>', { 'class': 'zm-command' })
+                        .append($('<a>', { id: 'izhRefresh', 'class': 'zg-btn-blue', href: 'javascript:void(0);', 'html': '刷新网页' }))
+                    )
+                )
+            ).appendTo(_doc.body).draggable({handle: '.modal-dialog-title-draggable'})
 
-    $(domBtnSettings).click(function(){
-        var $settings = $('#izh-dlg-settings')
-        $('.modal-dialog-bg').show()
-        $('input.izh-check', '#izh-dlg-settings').each(function(i, e){
-            var key = e.getAttribute('name')
-              , value = utils.getCfg(key)
-            if(value)
-                $(e).iCheck('check')
-        })
-        $settings.css({
-            'z-index':'89'
-          , 'position':'fixed'
-          , 'top': ($win.height() - $settings.height()) / 2
-          , 'left': ($win.width() - $settings.width()) / 2
-        }).fadeIn('slow')
-    }).insertBefore($('ul#top-nav-profile-dropdown li:last'))
-
-    var $dlg=$(domDlgSettings).appendTo(_doc.body)
-    $dlg.draggable({handle: '.modal-dialog-title-draggable'})
-    $('.modal-dialog-title-close',$dlg).click(function(){
-        $('.modal-dialog-bg').hide()
-        $('#izh-dlg-settings').first().hide()
-    })
-    $('input.izh-check', $dlg).each(function(i, e){
+    $('input.izh-check', $dlgSettings).each(function(i, e){
         var key = e.getAttribute('name')
           , $chk = $(e).iCheck({
                 checkboxClass: 'icheckbox_square-blue'
